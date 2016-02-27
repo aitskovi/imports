9abae7a92802b211b254d809c4318ff4099efdab
Avi Itskovich <aitskovi@stripe.com>
2016-02-27 08:39:02 -0800

    WIP

--- lib/imports.rb
+++ lib/imports.rb
@@ -1,8 +1,6 @@
 require "imports/version"
 
 # Stop ourselves from clobbering the import code.
-unless defined? ::IMPORTS_MODULES
-
 #TODO: Deal with circular dependancies
 
 ::IMPORTS_MODULES = {}
@@ -11,6 +9,8 @@ unless defined? ::IMPORTS_MODULES
 def import(path)
   base_path = caller_locations.first.absolute_path
   absolute_path = File.expand_path(path, "#{base_path}/..")
+
+  puts '#{base_path} is importing #{path}'
 
   unless ::IMPORTS_MODULES.include?(absolute_path)
     # Load the imported module, it'll register itself with the exports.
@@ -23,12 +23,12 @@ end
 def export(objects)
   absolute_path = caller_locations.first.absolute_path
 
+  puts '#{absolute_path} is exporting #{objects}'
+
   if ::IMPORTS_MODULES[absolute_path]
     raise RuntimeError.new('Doubly imported file')
   end
 
   ::IMPORTS_ANONYMOUS_MODULES[absolute_path] = self
   ::IMPORTS_MODULES[absolute_path] = objects
-end
-
 end
